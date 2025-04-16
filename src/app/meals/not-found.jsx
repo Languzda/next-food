afa4758912c4b6d2c77b06091d272f10bb03aa68
Languzda <langusda@gmail.com>
2025-04-16 17:21:45 +0200

    display details about meal

---
+++ src/app/meals/not-found.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <p>Please check the URL or return to the home page.</p>
+    </main>
+  );
+};
+
+export default NotFound;
